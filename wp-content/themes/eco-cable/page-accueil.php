60bd4861650329cbb2fa6547316bc9e70b630179
Unknown <alexis.ostermann67@gmail.com>
2018-03-10 23:50:54 +0100

    Modifs accueil + création "notre activité"

--- wp-content/themes/eco-cable/page-accueil.php
+++ wp-content/themes/eco-cable/page-accueil.php
@@ -4,8 +4,9 @@
     <?php
         while ( have_posts() ) : the_post();
         $titles = get_field('titles_group');
+        $bg_img = get_field('background_title');
     ?>
-        <div class="titles" style="<?php  ?>">
+        <div class="titles" style="background: linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(<?php echo $bg_img['url']; ?>);">
             <h1><?= $titles['title']; ?></h1>
             <h2><?= $titles['subtitle']; ?></h2>
         </div>
---
+++ wp-content/themes/eco-cable/page-notre-activite.php
@@ -0,0 +1,45 @@
+<?php get_header(); ?>
+
+<main class="wrapper notre-activite">
+    <?php
+        while ( have_posts() ) : the_post();
+        $achat_bg_img = get_field('achat_bg_title');
+        $transfo_bg_img = get_field('transfo_bg_title');
+        $vente_bg_img = get_field('$vente_bg_title');
+    ?>
+    <!-- ACHAT -->
+    <section>
+        <div class="titles" style="background: linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(<?php echo $achat_bg_img['url']; ?>);">
+            <h1><?php the_field('achat_title') ?></h1>
+        </div>
+        <div class="content">
+            <?php the_field('achat_content'); ?>
+        </div>
+    </section>
+    <!-- TRANSFORMATION -->
+    <section>
+        <div class="titles" style="background: linear-gradient(rgba(255,255,255,0.7), rgba(255,255,255,0.7)), url(<?php echo $transfo_bg_img['url']; ?>);">
+            <h1><?php the_field('transfo_title') ?></h1>
+        </div>
+        <div class="content">
+            <?php the_field('content_1'); ?>
+        </div>
+        <div class="process">
+            <?php if( have_rows('images_traitement') ): while ( have_rows('images_traitement') ) : the_row(); ?>
+                <div class="process-item">
+                    <?= wp_get_attachment_image(get_sub_field('image')); ?>
+                    <p class="img_subtitle"><?php the_sub_field('img_subtitle'); ?></p>
+                </div>
+            <?php endwhile; endif; ?>
+        </div>
+    </section>
+    <!-- VENTE -->
+    <section>
+
+    </section>
+    <?php
+        endwhile;
+    ?>
+</main>
+
+<?php get_footer(); ?>
